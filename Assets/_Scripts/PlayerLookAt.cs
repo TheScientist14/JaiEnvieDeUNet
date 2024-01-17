d8a024bac3bdae1640c008bd85deb69e0769886b
MrNyvle <elvynwakeford@gmail.com>
2024-01-17 15:52:16 +0100

    Eyes fixed ?

---
+++ Assets/_Scripts/PlayerLookAt.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerLookAt : NetworkBehaviour
+{
+    [SerializeField] private Transform camera;
+
+    private void Start()
+    {
+        if (!IsOwner)
+        {
+            gameObject.layer = 0;
+            return;
+        }
+        gameObject.layer = 8;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(camera.forward);
+    }
+}
